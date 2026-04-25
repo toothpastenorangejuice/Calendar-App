deb1289c4bbe9efccae3bab75ba3561b1b3e1e1c
MosaSelela <145341687+MosaSelela@users.noreply.github.com>
2026-04-25 18:21:49 +0200

    Calendar app, begining parts

---
+++ calendar-app/main.js
@@ -0,0 +1,32 @@
+/* *****************************************************************************/
+/*  Title: Nasha Wanich source code
+    Author: Wanich, N
+    Date: 2026
+    Availability: https://github.com/nasha-wanich
+*/
+/* *****************************************************************************/
+
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 292,
+    height: 430,
+    resizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    frame: false, 
+    transparent: false,
+    webPreferences: {
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
