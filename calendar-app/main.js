ce2d6bc7c636b32fd9ee3a44ceb2eaaadd314cd3
MosaSelela <145341687+MosaSelela@users.noreply.github.com>
2026-04-27 16:53:39 +0200

    added outer background image

--- calendar-app/main.js
+++ calendar-app/main.js
@@ -16,7 +16,7 @@ function createWindow() {
     maximizable: false,
     fullscreenable: false,
     frame: false, 
-    transparent: false,
+    transparent: true,
     webPreferences: {
       contextIsolation: true
     }
